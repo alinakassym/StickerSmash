ad9f02b6d3bcb4dffa8115016c0434788d8984b5
alinakassym <alinakassym@gmail.com>
2025-02-16 18:31:23 +0500

    feat: change app colors

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import { Link, Stack } from 'expo-router';
 
 export default function NotFoundScreen() {
@@ -6,6 +6,7 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'Oops! Not Found' }} />
       <View style={styles.container}>
+        <Text style={styles.text}>Oops! Not Found</Text>
         <Link href="/" style={styles.button}>
           Go back to Home screen!
         </Link>
@@ -17,14 +18,16 @@ export default function NotFoundScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7A1FA0',
+    backgroundColor: '#FFFFFF',
     justifyContent: 'center',
     alignItems: 'center',
   },
-
+  text: {
+    color: '#7A1FA0',
+  },
   button: {
     fontSize: 20,
     textDecorationLine: 'underline',
-    color: '#fff',
+    color: '#7A1FA0',
   },
 });
